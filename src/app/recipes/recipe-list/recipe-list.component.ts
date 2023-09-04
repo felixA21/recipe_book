56b85d4a00a79f9865f7959932763920e7cb8004
Felix Alimin <felixalimin@gmail.com>
2023-09-04 11:34:44 -0400

    fix img on second test recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -15,7 +15,11 @@ export class RecipeListComponent implements OnInit {
       'this is simply a test',
       'https://feelgoodfoodie.net/wp-content/uploads/2018/10/Shakshuka-09.jpg'
     ),
-    new Recipe('second recipe', 'second Test', 'alt'),
+    new Recipe(
+      'second recipe',
+      'second Test',
+      'https://hips.hearstapps.com/hmg-prod/images/comfort-food-recipes-64668a179574d.png'
+    ),
   ];
 
   constructor() {}
