45cbc43c271d818dc913dfe2125706e797080318
Felix Alimin <felixalimin@gmail.com>
2023-09-09 11:50:36 -0400

    enabled clear button on recipe edit

--- src/app/recipes/recipe-details/recipe-details.component.ts
+++ src/app/recipes/recipe-details/recipe-details.component.ts
@@ -33,4 +33,8 @@ export class RecipeDetailsComponent implements OnInit {
   onEditRecipe() {
     this.router.navigate(['edit'], { relativeTo: this.route });
   }
+
+  onDeleteRecipe() {
+    this.recipeService.deleteRecipe(this.id);
+  }
 }
